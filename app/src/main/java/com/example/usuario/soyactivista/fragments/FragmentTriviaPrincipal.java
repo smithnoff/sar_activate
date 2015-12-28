ba8865ec85db2ef4ad735da86862c6c20bbe69c6
unknown <Luis Aramburu>
2015-12-28 16:09:11 -0430

    Progress Button in FragmentTriviaPrincipal and add new library.

--- app/src/main/java/com/example/usuario/soyactivista/fragments/FragmentTriviaPrincipal.java
+++ app/src/main/java/com/example/usuario/soyactivista/fragments/FragmentTriviaPrincipal.java
@@ -1,25 +1,30 @@
 package com.example.usuario.soyactivista.fragments;
 
+import com.dd.processbutton.iml.ActionProcessButton;
+import logica.ProgressGenerator;
 
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import android.widget.Toolbar;
+
 
 
 
 import soy_activista.quartzapp.com.soy_activista.R;
 
+import static android.widget.Toast.*;
+
 /**
  * Created by Luis Adrian on 28/12/2015.
  */
-public class FragmentTriviaPrincipal extends Fragment{
+public class FragmentTriviaPrincipal extends Fragment implements ProgressGenerator.OnCompleteListener{
 
-    private Toolbar appbar;
+    public static final String EXTRAS_ENDLESS_MODE = "EXTRAS_ENDLESS_MODE";
 
 
     @Override
@@ -28,11 +33,26 @@ public class FragmentTriviaPrincipal extends Fragment{
         //Choose fragment to inflate
         View v = inflater.inflate(R.layout.fragment_trivia_principal, container, false);
 
-        appbar = (Toolbar)v.findViewById(R.id.appbar);
+        final ProgressGenerator progressGenerator = new ProgressGenerator(this);
+        final ActionProcessButton btnSignIn = (ActionProcessButton) v.findViewById(R.id.btnSignIn);
+
+        btnSignIn.setMode(ActionProcessButton.Mode.PROGRESS);
+
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressGenerator.start(btnSignIn);
+                btnSignIn.setEnabled(false);
+            }
+        });
 
         return v;
     }
 
-
+    @Override
+    public void onComplete() {
+        //Toast.makeText (getContext(), R.string.carga_completa, Toast.LENGTH_LONG).show();
+    }
 
 }
