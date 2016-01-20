5d4449412f9f0f6354175f7f49b532ddbfbcbaaa
smithnoff <cesar_abs9@hotmail.com>
2016-01-20 00:35:52 -0430

    mapa en vector venezuela

--- app/src/main/java/com/example/usuario/soyactivista/fragments/FragmentAAA.java
+++ app/src/main/java/com/example/usuario/soyactivista/fragments/FragmentAAA.java
@@ -1,23 +1,11 @@
 package com.example.usuario.soyactivista.fragments;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 import soy_activista.quartzapp.com.soy_activista.R;
 
@@ -32,7 +20,7 @@ private ImageView bolivar;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.aaexample, container, false);
-        bolivar = (ImageView)v.findViewById(R.id.bolivar);
+        bolivar = (ImageView)v.findViewById(R.id.imageViewVen);
         //bolivar.setColorFilter(R.color.red900);
         //bolivar.setColorFilter(R.color.black);
         //loadImageFromURL("http://vignette3.wikia.nocookie.net/future/images/7/76/Flag_of_Venezuela.png",bolivar);
